1008558f7c1221bcf00016bfd7f6e882f96d5ebe
Francis Appels <francis.appels@yahoo.com>
2013-11-09 15:59:23 +0100

    New tables for extdirect connector
    extdirect_user for authentication
    extdirect_activity for app activity tracking and locking
    Data in old user_app table isn't used anymore, table can be deleted

---
+++ extdirect/sql/llx_extdirect_activity.sql
@@ -0,0 +1,30 @@
+-- ============================================================================
+-- 
+-- Copyright (C) 2013      Francis Appels        <francis.appels@z-application.com>
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program. If not, see <http://www.gnu.org/licenses/>.
+--
+-- ============================================================================
+
+
+CREATE TABLE `llx_extdirect_activity` (
+  `rowid` int(11) NOT NULL auto_increment PRIMARY KEY,
+  `tms` timestamp,
+  `fk_user` int(11) default NULL,
+  `app_id` varchar(50) NOT NULL COMMENT 'extdirect app id',
+  `activity_name` varchar(50) NOT NULL COMMENT 'Name of activity or module',
+  `activity_id` int(11) NOT NULL DEFAULT 0 COMMENT 'rowid of module data, ex product or commande', 
+  `datec` datetime default NULL,
+  `status` varchar(50) default NULL
+) ENGINE=innodb;
---
+++ extdirect/sql/llx_extdirect_user.sql
@@ -0,0 +1,34 @@
+-- ============================================================================
+-- 
+-- Copyright (C) 2012      Francis Appels        <francis.appels@z-application.com>
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program. If not, see <http://www.gnu.org/licenses/>.
+--
+-- ============================================================================
+
+
+CREATE TABLE `llx_extdirect_user` (
+  `rowid` int(11) NOT NULL auto_increment PRIMARY KEY,
+  `tms` timestamp,
+  `fk_user` int(11) default NULL,
+  `app_id` varchar(50) NOT NULL COMMENT 'app id',
+  `app_name` varchar(50) NOT NULL,
+  `ack_id` varchar(50) default NULL COMMENT 'access key to send back to app',
+  `requestid` varchar(50) default NULL COMMENT 'Identification of the connectionon requestor',
+  `datec` datetime default NULL,
+  `date_last_connect` datetime default NULL,
+  `dev_platform` varchar(50) default NULL,
+  `dev_type` varchar(50) default NULL,
+  `import_key` varchar(14)
+) ENGINE=innodb;
