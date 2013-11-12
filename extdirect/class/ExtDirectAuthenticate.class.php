90e6dc3ed286ddce373b86901e09af4313d131f4
Francis Appels <francis.appels@yahoo.com>
2013-11-12 23:05:09 +0100

    Comment change

--- extdirect/class/ExtDirectAuthenticate.class.php
+++ extdirect/class/ExtDirectAuthenticate.class.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright (C) 2012		Francis Appels <francis.appels@z-application.com>
+ * Copyright (C) 2013		Francis Appels <francis.appels@z-application.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
