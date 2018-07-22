dbc512d237d993cdbde98677759bb28890e7f389
fappels <francis.appels@yahoo.com>
2018-07-22 17:50:18 +0200

    Tested for 8.0 compatibility

--- extdirect/class/extdirect.class.php
+++ extdirect/class/extdirect.class.php
@@ -486,13 +486,13 @@ class ExtDirect
         if($validate) 
         {
             $minVersion = '3.3';
-            $maxVersion = '7.0'; // tested version
+            $maxVersion = '8.0'; // tested version
         }
         if (empty($minVersion) && empty($maxVersion)) {
             return $dolMajorMinorVersion;
         } else {
             if (empty($minVersion)) $minVersion = '3.3';
-            if (empty($maxVersion)) $maxVersion = '7.0'; // debugging version
+            if (empty($maxVersion)) $maxVersion = '8.0'; // debugging version
             if (version_compare($minVersion, $dolMajorMinorVersion, '<=') && version_compare($maxVersion, $dolMajorMinorVersion, '>=')) 
             {
             	return 1;
