fee2fa04bf578a499711bbfb5b08f7bdab9ab780
Francis Appels <francis.appels@yahoo.com>
2019-12-19 16:44:54 +0100

    Tested on Dolibarr 11

--- extdirect/class/extdirect.class.php
+++ extdirect/class/extdirect.class.php
@@ -490,7 +490,7 @@ class ExtDirect
         if($validate)
         {
             $minVersion = '3.4';
-            $maxVersion = '10.0'; // tested version
+            $maxVersion = '11.0'; // tested version
         }
         if (empty($minVersion) && empty($maxVersion)) {
             return $dolMajorMinorVersion;
