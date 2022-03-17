26761f9130c98d065dbd0f91aaca34b5aac0f1a4
Francis Appels <francis.appels@yahoo.com>
2022-03-17 09:10:34 +0100

    Start on Dolibarr 16 test

--- extdirect/class/extdirect.class.php
+++ extdirect/class/extdirect.class.php
@@ -560,7 +560,7 @@ class ExtDirect
 			return $dolMajorMinorVersion;
 		} else {
 			if (empty($minVersion)) $minVersion = '3.8';
-			if (empty($maxVersion)) $maxVersion = '15.0'; // debugging version
+			if (empty($maxVersion)) $maxVersion = '16.0'; // debugging version
 			if (version_compare($minVersion, $dolMajorMinorVersion, '<=') && version_compare($maxVersion, $dolMajorMinorVersion, '>=')) {
 				return 1;
 			} else {
