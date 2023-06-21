4c3b318b928853017c5bf622c812de74ce226b87
Francis Appels <francis.appels@yahoo.com>
2023-06-21 10:28:38 +0200

    Fix activities list when in multicompany transverse mode.

--- extdirect/admin/extdirect.php
+++ extdirect/admin/extdirect.php
@@ -223,11 +223,6 @@ if ($mode == $tabs['tab1']->mode) {
 		if (!empty($conf->multicompany->enabled) && !empty($conf->global->MULTICOMPANY_TRANSVERSE_MODE)) {
 			print $extDirect->selectdolusers($conf->global->DIRECTCONNECT_AUTO_USER, 'userid', 1, $userExclude, 0, '', '');
 		} else {
-			if (!empty($conf->multicompany->enabled)) {
-				$extUser = new User($db);
-				$extUser->fetch($userId);
-				if ($extDirect->entity != $extUser->entity) $userId = -1; // entity mismatch
-			}
 			print $form->select_dolusers($conf->global->DIRECTCONNECT_AUTO_USER, 'userid', 1, $userExclude, 0, '', '');
 		}
 		print '</td><td align="right"><input type="submit" class="button" value="'.$langs->trans("Modify").'"></td>';
@@ -324,16 +319,7 @@ if ($mode == $tabs['tab1']->mode) {
 	print '<tr '.$bc[$var].'>';
 	print '<td>'.$langs->trans("ActivitiesFromUser").'</td>';
 	print '<td>';
-	if (!empty($conf->multicompany->enabled) && !empty($conf->global->MULTICOMPANY_TRANSVERSE_MODE)) {
-		print $extDirect->selectdolusers($userId, 'userid', 1, $userExclude, 0, '', '');
-	} else {
-		if (!empty($conf->multicompany->enabled)) {
-			$extUser = new User($db);
-			$extUser->fetch($userId);
-			if ($extDirect->entity != $extUser->entity) $userId = -1; // entity mismatch
-		}
-		print $form->select_dolusers($userId, 'userid', 1, $userExclude, 0, '', '');
-	}
+	print $form->select_dolusers($userId, 'userid', 1, $userExclude, 0, '', '');
 	print '</td><td></td><td></td><td></td><td></td><td>';
 	print '<input type="submit" name="refresh" class="button" value="'.$langs->trans("Refresh").'">';
 	print '</td>';
