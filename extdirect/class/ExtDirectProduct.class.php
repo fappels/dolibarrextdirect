9be464cab59fdb63c93fc051292236ad3a44d961
Francis Appels <francis.appels@yahoo.com>
2025-04-16 16:46:49 +0200

    Add filter to force load virtual stock. If load for supplier, also add draft order stock. Add flag to indicate total_stock is virtual stock.

--- extdirect/class/ExtDirectProduct.class.php
+++ extdirect/class/ExtDirectProduct.class.php
@@ -169,6 +169,7 @@ class ExtDirectProduct extends ProductFournisseur
 				elseif ($filter->property == 'ref_supplier_id') $refSupplierId = $filter->value;
 				elseif ($filter->property == 'photo_size' && !empty($filter->value)) $photoSize = $filter->value;
 				elseif ($filter->property == 'customer_id' && !empty($filter->value)) $socid = $filter->value;
+				elseif ($filter->property == 'get_virtual_stock' && !empty($filter->value)) $conf->global->STOCK_SHOW_VIRTUAL_STOCK_IN_PRODUCTS_COMBO = $filter->value;
 			}
 		}
 
@@ -186,6 +187,7 @@ class ExtDirectProduct extends ProductFournisseur
 				$this->fetch_barcode();
 				$row->id = $this->id ;
 				$row->is_virtual_stock = false;
+				$row->is_virtual_total_stock = false;
 				//! Ref
 				$row->ref= $this->ref;
 				$row->label= $this->label;
@@ -265,7 +267,11 @@ class ExtDirectProduct extends ProductFournisseur
 
 				//! Stock
 				if (isset($warehouse) && $warehouse != ExtDirectFormProduct::ALLWAREHOUSE_ID) {
-					$this->load_stock('novirtual, warehouseopen, warehouseinternal');
+					if (!empty($conf->global->STOCK_SHOW_VIRTUAL_STOCK_IN_PRODUCTS_COMBO)) {
+						$this->load_stock('warehouseopen, warehouseinternal', ($refSupplierId > 0) ? 1 : null);
+					} else {
+						$this->load_stock('novirtual, warehouseopen, warehouseinternal');
+					}
 					$row->pmp = $this->pmp;
 					if (!empty($conf->productbatch->enabled) && (!empty($batch) || isset($batchId))) {
 						// TODO if warehouse is a parent warehouse get all batches from childs
@@ -309,7 +315,7 @@ class ExtDirectProduct extends ProductFournisseur
 						// fetch qty and warehouse of first batch found
 						$formProduct = new FormProduct($this->db);
 						if (!empty($conf->global->STOCK_SHOW_VIRTUAL_STOCK_IN_PRODUCTS_COMBO)) {
-							$this->load_stock('warehouseopen, warehouseinternal');
+							$this->load_stock('warehouseopen, warehouseinternal', ($refSupplierId > 0) ? 1 : null);
 						} else {
 							$this->load_stock('novirtual, warehouseopen, warehouseinternal');
 						}
@@ -340,7 +346,7 @@ class ExtDirectProduct extends ProductFournisseur
 						}
 					} else {
 						if (!empty($conf->global->STOCK_SHOW_VIRTUAL_STOCK_IN_PRODUCTS_COMBO)) {
-							$this->load_stock('warehouseopen, warehouseinternal');
+							$this->load_stock('warehouseopen, warehouseinternal', ($refSupplierId > 0) ? 1 : null);
 							$row->is_virtual_stock = true;
 							$row->stock_reel = (float) $this->stock_theorique;
 						} else {
@@ -360,6 +366,7 @@ class ExtDirectProduct extends ProductFournisseur
 				// add compatibility with orderline model
 				$row->stock = $row->stock_reel;
 				if (!empty($conf->global->STOCK_SHOW_VIRTUAL_STOCK_IN_PRODUCTS_COMBO)) {
+					$row->is_virtual_total_stock = true;
 					$row->total_stock = (float) $this->stock_theorique;
 				} else {
 					$row->total_stock = (float) $this->stock_reel;
@@ -1441,6 +1448,7 @@ class ExtDirectProduct extends ProductFournisseur
 					if ($filter->value > 0) $warehouseIds[] = $filter->value;
 					$checkWarehouseIds[] = $filter->value;
 				}
+				elseif ($filter->property == 'get_virtual_stock' && !empty($filter->value)) $conf->global->STOCK_SHOW_VIRTUAL_STOCK_IN_PRODUCTS_COMBO = $filter->value;
 			}
 		}
 		if (isset($param->include_total)) {
@@ -1646,10 +1654,10 @@ class ExtDirectProduct extends ProductFournisseur
 				} else {
 					$row->stock = (float) $obj->stock;
 				}
-				if (!empty($conf->global->STOCK_SHOW_VIRTUAL_STOCK_IN_PRODUCTS_COMBO) && !$warehouseFilter) {
+				if (!empty($conf->global->STOCK_SHOW_VIRTUAL_STOCK_IN_PRODUCTS_COMBO) && empty($warehouseIds)) {
 					$product = new Product($this->db);
 					$product->fetch($row->product_id);
-					$product->load_stock('warehouseopen, warehouseinternal');
+					$product->load_stock('warehouseopen, warehouseinternal', $supplierFilter ? 1 : null);
 					$row->is_virtual_stock = true;
 					$row->total_stock = (float) $product->stock_theorique;
 				} else {
